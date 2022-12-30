17d2b096fb9399f81be22ac5224a9c53ad431fe3
leo <leo.studenikin@gmail.com>
2022-12-30 19:44:31 +0300

    default role prop partner on new registrations added to model and action

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -30,6 +30,7 @@ class CreateNewUser implements CreatesNewUsers
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
+            'role' => 'partner',
             'password' => Hash::make($input['password']),
         ]);
     }
