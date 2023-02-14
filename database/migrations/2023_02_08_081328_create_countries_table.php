5632437bfb33fd991d8b5799cf3230744af566f8
leo <leo.studenikin@gmail.com>
2023-02-14 20:47:49 +0300

    countries seeder, data, model - iso, dialcode, currency, phone mask

---
+++ database/migrations/2023_02_08_081328_create_countries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('iso', 2);
+            $table->string('dial_code', 5);
+            $table->string('currency', 3)->nullable();
+            $table->string('mask', 20)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+};
---
+++ database/seeders/CountrySeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use DB;
+use Storage;
+use Illuminate\Database\Seeder;
+
+class CountrySeeder extends Seeder
+{
+    /**
+     * Run the database seeds. php artisan db:seed --class=CountrySeeder
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (!Storage::disk('seeders')->exists('data/countries_full.json')) {
+            dump('data/countries_full.json file does not exist!');
+            return;
+        }
+        $countries = json_decode(Storage::disk('seeders')->get('/data/countries_full.json'));
+        foreach ($countries as $country) {
+            DB::table('countries')->insert([
+                'name' => $country->name,
+                'iso' => $country->iso,
+                'dial_code' => $country->dial_code ?? null,
+                'currency' => $country->currency,
+                'mask' => (is_array($country->mask) ? $country->mask[0] : $country->mask),
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
+    }
+}
