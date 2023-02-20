aed1cfe848845efbb58ad7544f051ae02c54fd8f
leo <leo.studenikin@gmail.com>
2023-02-20 13:15:31 +0300

    inertia middleware update - removal of menu

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 use Route;
+// use Tightenco\Ziggy\Ziggy;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -40,64 +41,12 @@ class HandleInertiaRequests extends Middleware
         return array_merge(parent::share($request), [
             'app_name' => config('app.name'),
             'route_name' => Route::currentRouteName(),
-            'menu' => $this->navRoutes($request->user()),
             'flash' => [
                 'type' => $request->session()->get('flash_type'),
                 'message' => $request->session()->get('flash_message', __('OK')),
                 'timestamp' => $request->session()->get('flash_timestamp'),
             ],
+            // 'ziggy' => (new Ziggy(['common', 'partner', 'admin']))->toArray(),
         ]);
-    }
-
-    public function navRoutes($user): array
-    {
-        if(empty($user)){
-            return [];
-        }
-
-        if($user->is_partner){
-            return array(
-                [
-                    'title' => 'Dashboard',
-                    'icon' => 'fa-solid fa-home',
-                    'route_name' => 'partner.dashboard',
-                ],
-                [
-                    'title' => 'Pricing',
-                    'icon' => 'fa-solid fa-gears',
-                    'route_name' => 'partner.pricing.index',
-                ],
-            );
-        }
-
-        if($user->is_admin){
-            return array(
-                [
-                    'title' => 'Dashboard',
-                    'icon' => 'fa-solid fa-home',
-                    'route_name' => 'admin.dashboard',
-                ],
-                [
-                    'title' => 'Partner performance',
-                    'icon' => 'fa-solid fa-gauge-high',
-                    'route_name' => 'admin.partners.performance.index',
-                ],
-                [
-                    'title' => 'Partner management',
-                    'icon' => 'fa-solid fa-user-tie',
-                    'route_name' => 'admin.partners.index',
-                ],
-                [
-                    'title' => 'Settings',
-                    'icon' => 'fa-solid fa-gears',
-                    'route_name' => 'admin.settings',
-                ],
-                [
-                    'title' => 'Typography',
-                    'icon' => 'fa-solid fa-coffee',
-                    'route_name' => 'admin.demo',
-                ],
-            );
-        }
     }
 }
