7faaa9b535b1db4900f06bad0efec86ce6aa25c3
leo <leo.studenikin@gmail.com>
2023-03-17 19:13:02 +0300

    cardicon component, icon slot class prop added

--- resources/js/Components/CardIcon.vue
+++ resources/js/Components/CardIcon.vue
@@ -2,13 +2,14 @@
 import { Link } from '@inertiajs/vue3';
 const props = defineProps({
     cardLink: String,
+    iconClass: String,
 });
 </script>
 
 <template>
     <div class="bg-gray-50 flex hover:bg-gray-100 items-center p-2 gap-4 relative transition">
         <Link v-if="cardLink" :href="cardLink" class="absolute bottom-0 top-0 w-full cursor-pointer"></Link>
-        <div v-if="$slots.icon" class="flex-shrink-0">
+        <div v-if="$slots.icon" class="flex-shrink-0" :class="iconClass">
             <slot name="icon"></slot>
         </div>
         <div class="flex-1 min-w-0">
