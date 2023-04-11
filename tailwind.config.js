3043638041dc7ca669fe876da512babf7fc3b98f
leo <leo.studenikin@gmail.com>
2023-04-11 17:51:58 +0300

    vue packages and tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const svgToDataUri = require('mini-svg-data-uri');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -12,6 +13,19 @@ module.exports = {
 
     theme: {
         extend: {
+            backgroundImage: (theme) => ({
+                'multiselect-caret': `url("${svgToDataUri(
+                    `<svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.00048 3.78047L8.30048 0.480469L9.24315 1.42314L5.00048 5.6658L0.757812 1.42314L1.70048 0.480469L5.00048 3.78047Z" fill="#C1C9DE"/>
+                    </svg>`,
+                )}")`,
+                'multiselect-spinner': `url("${svgToDataUri(
+                    `<svg viewBox="0 0 512 512" fill="${theme('colors.green.500')}" xmlns="http://www.w3.org/2000/svg"><path d="M456.433 371.72l-27.79-16.045c-7.192-4.152-10.052-13.136-6.487-20.636 25.82-54.328 23.566-118.602-6.768-171.03-30.265-52.529-84.802-86.621-144.76-91.424C262.35 71.922 256 64.953 256 56.649V24.56c0-9.31 7.916-16.609 17.204-15.96 81.795 5.717 156.412 51.902 197.611 123.408 41.301 71.385 43.99 159.096 8.042 232.792-4.082 8.369-14.361 11.575-22.424 6.92z"></path></svg>`,
+                )}")`,
+                'multiselect-remove': `url("${svgToDataUri(
+                    `<svg viewBox="0 0 320 512" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M207.6 256l107.72-107.72c6.23-6.23 6.23-16.34 0-22.58l-25.03-25.03c-6.23-6.23-16.34-6.23-22.58 0L160 208.4 52.28 100.68c-6.23-6.23-16.34-6.23-22.58 0L4.68 125.7c-6.23 6.23-6.23 16.34 0 22.58L112.4 256 4.68 363.72c-6.23 6.23-6.23 16.34 0 22.58l25.03 25.03c6.23 6.23 16.34 6.23 22.58 0L160 303.6l107.72 107.72c6.23 6.23 16.34 6.23 22.58 0l25.03-25.03c6.23-6.23 6.23-16.34 0-22.58L207.6 256z"></path></svg>`,
+                )}")`,
+            }),
             fontFamily: {
                 sans: ['Urbanist', ...defaultTheme.fontFamily.sans],
             },
