03ab277c03d8139a4656d7f5c6abb9fc4458503a
leo <leo.studenikin@gmail.com>
2023-06-19 11:08:08 +0300

    inertia version 1.0.8 update; layout templates head fix

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -64,31 +64,16 @@ let back = function (e) {
 <template>
     <div>
         <AppHead :title="$page.props.page_title">
-            <Link
-                rel="apple-touch-icon"
-                sizes="180x180"
-                href="/apple-touch-icon.png"
-            />
-            <Link
-                rel="icon"
-                type="image/png"
-                sizes="32x32"
-                href="/favicon-32x32.png"
-            />
-            <Link
-                rel="icon"
-                type="image/png"
-                sizes="16x16"
-                href="/favicon-16x16.png"
-            />
-            <Link rel="manifest" href="/site.webmanifest" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+            <link rel="manifest" href="/site.webmanifest">
         </AppHead>
 
         <Banner />
         <FlashMessage
             :flash="$page.props.flash"
             :errors="$page.props.errors"
-            :show-at="$page.props.show_class"
         />
 
         <div class="min-h-screen bg-gray-50 overflow-hidden">
@@ -107,22 +92,11 @@ let back = function (e) {
                             'translate-x-0': !sidebarCollapsed,
                         }"
                     >
-                        <div
-                            class="w-full md:w-56 xl:w-[220px] 2xl:w-[260px] h-full bg-primary-500"
-                        >
+                        <div class="w-full md:w-56 xl:w-[220px] 2xl:w-[260px] h-full bg-primary-500">
                             <div class="flex items-center flex-shrink-0">
                                 <!-- Logo -->
-                                <div
-                                    class="relative w-full px-6 py-4 xl:py-6 2xl:py-8"
-                                >
-                                    <Link
-                                        :href="
-                                            route(
-                                                $page.props.user.dashboard_route
-                                            )
-                                        "
-                                        class="mt-1"
-                                    >
+                                <div class="relative w-full px-6 py-4 xl:py-6 2xl:py-8">
+                                    <Link :href="route($page.props.user.dashboard_route)" class="mt-1">
                                         <LogoLetter
                                             class="w-auto h-10"
                                             fill="#fff"
@@ -153,13 +127,9 @@ let back = function (e) {
                     </div>
 
                     <!-- Main Content -->
-                    <div
-                        class="flex flex-col flex-1 overflow-hidden md:relative md:z-10 h-full md:h-auto"
-                    >
+                    <div class="flex flex-col flex-1 overflow-hidden md:relative md:z-10 h-full md:h-auto">
                         <!-- Page Heading / Menu item name-->
-                        <header
-                            class="bg-primary-500 md:bg-white flex items-center justify-between md:px-6 md:py-6 md:text-md p-4 shadow text-sm w-full gap-4 relative"
-                        >
+                        <header class="bg-primary-500 md:bg-white flex items-center justify-between md:px-6 md:py-6 md:text-md p-4 shadow text-sm w-full gap-4 relative">
                             <div class="flex items-center">
                                 <!-- Menu toggle -->
                                 <button
@@ -169,10 +139,7 @@ let back = function (e) {
                                         sidebarCollapsed = !sidebarCollapsed
                                     "
                                 >
-                                    <font-awesome-icon
-                                        :icon="faBars"
-                                        class="w-6 h-6 2xl:w-7 2xl:h-7"
-                                    />
+                                    <font-awesome-icon :icon="faBars" class="w-6 h-6 2xl:w-7 2xl:h-7" />
                                 </button>
                                 <!-- Mobile toggle, visible md and smaller -->
                                 <Dropdown
@@ -196,13 +163,8 @@ let back = function (e) {
                                     </template>
                                 </Dropdown>
                                 <div class="max-w-7xl mx-auto">
-                                    <h2
-                                        class="font-semibold text-base md:text-xl xl:text-2xl 2xl:text-3xl text-white md:text-gray-800 leading-tight"
-                                    >
-                                        <div
-                                            v-if="headerIsArray"
-                                            class="flex flex-wrap gap-2 items-center"
-                                        >
+                                    <h2 class="font-semibold text-base md:text-xl xl:text-2xl 2xl:text-3xl text-white md:text-gray-800 leading-tight">
+                                        <div v-if="headerIsArray" class="flex flex-wrap gap-2 items-center">
                                             <Link
                                                 href="#"
                                                 @click="back"
@@ -230,22 +192,14 @@ let back = function (e) {
                             </div>
                             <div class="flex items-center">
                                 <!-- Setting Links -->
-                                <ul
-                                    class="gap-5 pr-6 py-2 mr-5 border-r-2 border-gray-200 hidden md:flex"
-                                >
+                                <ul class="gap-5 pr-6 py-2 mr-5 border-r-2 border-gray-200 hidden md:flex">
                                     <li>
-                                        <a
-                                            href="#"
-                                            class="text-white md:text-dark"
-                                        >
+                                        <a href="#" class="text-white md:text-dark" >
                                             <Notifications />
                                         </a>
                                     </li>
                                     <li>
-                                        <a
-                                            href="#"
-                                            class="text-white md:text-dark"
-                                        >
+                                        <a href="#" class="text-white md:text-dark" >
                                             <Messages />
                                         </a>
                                     </li>
@@ -259,36 +213,16 @@ let back = function (e) {
                                         :content-classes="['bg-white']"
                                     >
                                         <template #trigger>
-                                            <div
-                                                v-if="
-                                                    $page.props.jetstream
-                                                        .managesProfilePhotos
-                                                "
-                                                class="flex items-center justify-center"
-                                            >
+                                            <div v-if="$page.props.jetstream.managesProfilePhotos" class="flex items-center justify-center">
                                                 <div class="pr-4 text-right">
-                                                    <div
-                                                        class="font-bold text-white md:text-dark md:text-sm xl:text-md"
-                                                    >
-                                                        {{
-                                                            $page.props.user
-                                                                .name
-                                                        }}
+                                                    <div class="font-bold text-white md:text-dark md:text-sm xl:text-md">
+                                                        {{$page.props.user.name}}
                                                     </div>
-                                                    <div
-                                                        class="text-gray-400 hidden md:block md:text-sm xl:text-md"
-                                                    >
-                                                        {{
-                                                            $page.props.user
-                                                                .email
-                                                        }}
+                                                    <div class="text-gray-400 hidden md:block md:text-sm xl:text-md">
+                                                        {{$page.props.user.email}}
                                                     </div>
                                                 </div>
-                                                <Avatar
-                                                    :title="
-                                                        $page.props.user.name
-                                                    "
-                                                />
+                                                <Avatar :title="$page.props.user.name"/>
                                                 <!-- <img
                                                         class="w-8 h-8 rounded-full object-cover"
                                                         :src="
@@ -299,10 +233,7 @@ let back = function (e) {
                                                     /> -->
                                             </div>
 
-                                            <span
-                                                v-else
-                                                class="inline-flex rounded-md"
-                                            >
+                                            <span v-else class="inline-flex rounded-md">
                                                 <button
                                                     type="button"
                                                     class="inline-flex items-center px-3 py-2 border border-transparent text-sm lg:text-md leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition"
@@ -326,9 +257,7 @@ let back = function (e) {
                                         </template>
 
                                         <template #content>
-                                            <div
-                                                class="px-4 py-2 border-b bg-gray-50 md:hidden"
-                                            >
+                                            <div class="px-4 py-2 border-b bg-gray-50 md:hidden">
                                                 <div class="font-bold">
                                                     {{ $page.props.user.name }}
                                                 </div>
@@ -338,9 +267,7 @@ let back = function (e) {
                                             </div>
                                             <!-- Account Management -->
 
-                                            <DropdownLink
-                                                :href="route('profile.show')"
-                                            >
+                                            <DropdownLink :href="route('profile.show')">
                                                 Profile
                                             </DropdownLink>
 
@@ -348,35 +275,15 @@ let back = function (e) {
                                                 API Tokens
                                             </DropdownLink> -->
 
-                                            <DropdownLink
-                                                v-if="
-                                                    $page.props.user.is_partner
-                                                "
-                                                :href="
-                                                    route(
-                                                        'partner.subscriptions.index'
-                                                    )
-                                                "
-                                            >
+                                            <DropdownLink v-if="$page.props.user.is_partner" :href="route('partner.subscriptions.index')">
                                                 Billing
                                             </DropdownLink>
 
-                                            <DropdownLink
-                                                v-if="
-                                                    $page.props.user.is_partner
-                                                "
-                                                :href="
-                                                    route(
-                                                        'partner.contact.index'
-                                                    )
-                                                "
-                                            >
+                                            <DropdownLink v-if="$page.props.user.is_partner" :href="route('partner.contact.index')">
                                                 Contact support
                                             </DropdownLink>
 
-                                            <div
-                                                class="border-t border-gray-100"
-                                            />
+                                            <div class="border-t border-gray-100"/>
 
                                             <!-- Authentication -->
                                             <form @submit.prevent="logout">
@@ -390,17 +297,13 @@ let back = function (e) {
                             </div>
                         </header>
                         <!-- Page Content -->
-                        <div
-                            class="md:flex-1 overflow-y-auto bg-mainBg h-full md:h-auto"
-                        >
+                        <div class="md:flex-1 overflow-y-auto bg-mainBg h-full md:h-auto">
                             <main class="py-8 px-4 sm:px-6">
                                 <slot />
                             </main>
                         </div>
                         <!-- footer -->
-                        <footer
-                            class="bg-gray-100 h-applayout_footer flex items-center justify-center text-center text-gray-400 text-xs"
-                        >
+                        <footer class="bg-gray-100 h-applayout_footer flex items-center justify-center text-center text-gray-400 text-xs">
                             MADE WITH
                             <font-awesome-icon :icon="faHeart" class="mx-2" />
                             IN LONDON
