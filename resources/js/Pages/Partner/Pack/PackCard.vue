8a27519d6c61bc92b50a86e66accce228cd6f268
leo <leo.studenikin@gmail.com>
2023-09-06 14:54:29 +0300

    admin packs update

--- resources/js/Pages/Partner/Pack/PackCard.vue
+++ resources/js/Pages/Partner/Pack/PackCard.vue
@@ -4,7 +4,7 @@ import { DateTime } from 'luxon';
 import { faEllipsis, faUserGroup, faPencil, faCopy, faTrashCan, faLock, faPowerOff, faMask, faCalendarDays } from '@fortawesome/free-solid-svg-icons';
 import ButtonLink from '@/Components/ButtonLink.vue';
 import DonutText from '@/Components/Charts/DonutText.vue';
-import Dropdown from '@/Components/Dropdown.vue';
+import { hideAllPoppers } from 'floating-vue';
 
 const props = defineProps({
     pack: {
@@ -39,108 +39,128 @@ defineEmits(['copy', 'toggle', 'delete']);
                 'border-cyan-500': pack.type == 'corporate',
             }">
         <div v-if="pack.type == 'hybrid'" class="absolute bg-gradient-to-r from-secondary-600 h-2 inset-x-0 top-[-8px] rounded-tl-md rounded-tr-md to-blue-600 w-full"></div>
-        <Dropdown align="right" width="48" :content-classes="['bg-gray-100', 'p-1', 'space-y-4']" class="h-6 mx-2 place-self-end w-6" :class="{'z-10': !pack.is_active}">
-            <template #trigger>
-                <ButtonLink>
-                    <font-awesome-icon :icon="faEllipsis" class="w-6 h-6" :class="{'text-white': !pack.is_active}" />
-                </ButtonLink>
+        <VDropdown placement="bottom-end" :distance="0" class="self-end">
+            <ButtonLink>
+                <font-awesome-icon :icon="faEllipsis" class="w-6 h-6 mx-2" :class="{'text-white': !pack.is_active, 'z-10': !pack.is_active}" />
+            </ButtonLink>
+            <template #popper>
+                <div class="p-2 w-40 space-y-4">
+                    <ButtonLink 
+                        :href="route('partner.packs.edit', pack)"
+                        size="small"
+                        class="flex justify-between w-full"
+                        styling="default"
+                        >
+                        <font-awesome-icon :icon="faPencil" />
+                        Edit
+                    </ButtonLink>
+                    <ButtonLink 
+                        @click="$emit('copy', pack.id); hideAllPoppers();"
+                        size="small"
+                        class="flex justify-between w-full"
+                        styling="secondary"
+                        >
+                        <font-awesome-icon :icon="faCopy" />
+                        Duplicate
+                    </ButtonLink>
+                    <ButtonLink 
+                        @click="$emit('toggle', pack.id); hideAllPoppers();"
+                        size="small"
+                        class="flex justify-between w-full"
+                        :styling="pack.is_active ? 'default' : 'primary'"
+                        >
+                        <font-awesome-icon :icon="faPowerOff" />
+                        {{pack.is_active ? 'Deactive' : 'Make active'}} 
+                    </ButtonLink>
+                    <ButtonLink 
+                        @click="$emit('delete', pack.id); hideAllPoppers();"
+                        size="small"
+                        class="flex justify-between w-full"
+                        styling="danger"
+                        >
+                        <font-awesome-icon :icon="faTrashCan" />
+                        Delete
+                    </ButtonLink>
+                </div>
             </template>
-            <template #content>
-                <ButtonLink 
-                    :href="route('partner.packs.edit', pack)"
-                    size="small"
-                    class="flex justify-between w-full"
-                    styling="default"
-                    >
-                    Edit
-                    <font-awesome-icon :icon="faPencil" />
-                </ButtonLink>
-                <ButtonLink 
-                    @click="$emit('copy', pack.id)"
-                    size="small"
-                    class="flex justify-between w-full"
-                    styling="secondary"
-                    >
-                    Duplicate
-                    <font-awesome-icon :icon="faCopy" />
-                </ButtonLink>
-                <ButtonLink 
-                    @click="$emit('toggle', pack.id)"
-                    size="small"
-                    class="flex justify-between w-full"
-                    :styling="pack.is_active ? 'default' : 'primary'"
-                    >
-                    {{pack.is_active ? 'Deactive' : 'Make active'}} 
-                    <font-awesome-icon :icon="faPowerOff" />
-                </ButtonLink>
-                <ButtonLink 
-                    @click="$emit('delete', pack.id)"
-                    size="small"
-                    class="flex justify-between w-full"
-                    styling="danger"
-                    type="button"
-                    >
-                    Delete
-                    <font-awesome-icon :icon="faTrashCan" />
-                </ButtonLink>
-            </template>
-        </Dropdown>
-        <div class="p-2 flex gap-4">
+        </VDropdown>
+        <div class="flex items-center">
             <DonutText 
-                    class="w-20 flex-shrink-0"
+                    class="w-24 flex-shrink-0 ml-1"
                     :percent="donutPercent"
                     :title="donutPercent+'%'"
                     :subtitle="'active'"
                 >
             </DonutText>
-            <div class="text-gray-500">
+            <div class="px-2 text-gray-500">
                 <div class="font-bold text-xl text-black leading-tight mb-2">{{pack.title}}</div>
-                <div class="mb-1 border-b">
-                    <font-awesome-icon :icon="faUserGroup" class="mr-2" />
-                    <span v-tooltip.bottom="'Pack total: ' + pack.memberships_count">
+                <VTooltip :triggers="['hover', 'click']" class="mb-2 border-b">
+                    <div>
+                        <font-awesome-icon :icon="faUserGroup" class="mr-2" />
                         {{pack.active_memberships_count}} active
-                    </span>
-                </div>
-                <div class="flex gap-4 items-center">
+
+                    </div>
+                    <template #popper>
+                        Pack total: {{pack.memberships_count}}
+                    </template>
+                </VTooltip>
+                <div class="flex gap-8 items-center">
                     <font-awesome-icon
                         v-if="pack.is_restricted"
                         :icon="faLock"
                         v-tooltip="'Pack has restrictions'" />
-                    <font-awesome-icon
-                        v-if="pack.is_private"
-                        :icon="faMask"
-                        v-tooltip="'Pack is private'" />
-                    <font-awesome-icon
-                        v-if="pack.not_yet_active === true"
-                        :icon="faCalendarDays"
-                        class="text-red-500"
-                        v-tooltip="'Pack will be available from '+
-                        DateTime.fromISO(pack.active_from)
-                        .setZone(business_settings.timezone)
-                        .toFormat(business_settings.date_format.format_js)" />
-                    <font-awesome-icon
-                        v-if="pack.will_become_inactive === true"
-                        :icon="faCalendarDays"
-                        class="text-secondary-500"
-                        v-tooltip="'Pack will be available until the end of '+
-                        DateTime.fromISO(pack.active_to)
-                        .setZone(business_settings.timezone)
-                        .toFormat(business_settings.date_format.format_js)" />
-                    <font-awesome-icon
-                        v-if="pack.became_inactive === true"
-                        :icon="faCalendarDays"
-                        class="text-red-500"
-                        v-tooltip="'Pack was available until the end of '+
-                        DateTime.fromISO(pack.active_to)
-                        .setZone(business_settings.timezone)
-                        .toFormat(business_settings.date_format.format_js)" />
+                    <VTooltip v-if="pack.is_private" :triggers="['hover', 'click']">
+                        <font-awesome-icon :icon="faMask" />
+                        <template #popper>
+                            Pack is private
+                        </template>
+                    </VTooltip>
+                    <VTooltip v-if="pack.not_yet_active === true" :triggers="['hover', 'click']" >
+                        <font-awesome-icon :icon="faCalendarDays" class="text-red-500" />
+                        <template #popper>
+                            <div class="w-72">
+                                Pack will be available from 
+                                {{DateTime.fromISO(pack.active_from)
+                                    .setZone(business_settings.timezone)
+                                    .toFormat(business_settings.date_format.format_js)}}
+                            </div>
+                        </template>
+                    </VTooltip>
+                    <VTooltip v-if="pack.will_become_inactive === true" :triggers="['hover', 'click']" >
+                        <font-awesome-icon :icon="faCalendarDays" class="text-secondary-500" />
+                        <template #popper>
+                            <div class="w-72">
+                                Pack will be available until the end of
+                                {{DateTime.fromISO(pack.active_to)
+                                    .setZone(business_settings.timezone)
+                                    .toFormat(business_settings.date_format.format_js)}}
+                            </div>
+                        </template>
+                    </VTooltip>
+                    <VTooltip v-if="pack.became_inactive === true" :triggers="['hover', 'click']" >
+                        <font-awesome-icon :icon="faCalendarDays" class="text-red-900" />
+                        <template #popper>
+                            <div class="w-72">
+                                Pack was available until the end of
+                                {{DateTime.fromISO(pack.active_to)
+                                    .setZone(business_settings.timezone)
+                                    .toFormat(business_settings.date_format.format_js)}}
+                            </div>
+                        </template>
+                    </VTooltip>
                 </div>
             </div>
         </div>
 
         <div v-if="pack.sub_title || pack.description" class="p-2">
-            <div v-tooltip.left="'pack.sub_title'" class="truncate">{{pack.sub_title}}</div>
-            <div v-tooltip="pack.description" class="truncate">{{pack.description}}</div>
+            <div class="truncate">{{pack.sub_title}}</div>
+            <VTooltip :triggers="['hover', 'click']">
+                <div class="truncate">{{pack.description}}</div>
+
+                <template #popper>
+                    <div class="w-72">{{pack.description}}</div>
+                </template>
+            </VTooltip>
         </div>
 
         <div class="bg-gray-50 border border-gray-50 font-bold place-self-start px-2 rounded-tr-lg text-sm mt-2">Pricing options</div>
