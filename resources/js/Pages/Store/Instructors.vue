2a0ccde2562d3566c3ddec35e8283416a919e6b0
AmirJavaidSwe <softwareengr.1@gmail.com>
2023-09-26 13:52:55 -0700

    change the content in insturctors page

--- resources/js/Pages/Store/Instructors.vue
+++ resources/js/Pages/Store/Instructors.vue
@@ -1,6 +1,8 @@
 <script setup>
 import Section from '@/Components/Section.vue';
 import ButtonLink from '@/Components/ButtonLink.vue';
+import InstructorNavMenu from '@/Layouts/InstructorNavMenu.vue';
+import { router, Link, usePage } from '@inertiajs/vue3';
 const props = defineProps({
     instructors: {
         type: Object,
@@ -11,15 +13,22 @@ const props = defineProps({
 
 <template>
     <Section bg="bg-transparent">
-        <div class="text-xl mb-4">
-            Our Instructors
+    <div class="mx-20">
+        <div class="mt-6 mb-6">
+            <p class="text-3xl  text-center  font-bold">Instructors</p>
+            <p class="text-center text-lg mb-6 font-bold">
+                Getting you powerd up
+            </p>
         </div>
-        <div class="flex flex-wrap gap-4 flex-1">
-            <div v-for="item in instructors" :key="item.id" class="bg-white p-4 rounded shadow-md flex-grow">
+        <InstructorNavMenu></InstructorNavMenu>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4">
+            <div v-for="item in instructors" :key="item.id" class=" flex-grow">
+                <img src="/images/gymwoman.jpg" alt="Instructor's Image" class="w-[300px] h-[250px] mb-2">
                 <div class="font-bold mb-2">{{item.full_name}}</div>
-                <div>email: {{item.email}}</div>
+                <div>CYCLE</div>
             </div>
         </div>
-
+    </div>
     </Section>
 </template>
+
